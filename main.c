11086b29dfcec635e028aa4aaa7157c989447c31
don1997 <donald_j97@hotmail.com>
2022-07-31 15:41:37 -0400

    fixed inf loop print bug and \n ERROR message

--- main.c
+++ main.c
@@ -45,31 +45,57 @@ object *make_error(){
 
 /*      READ        */
 
-int peek(void) { 
+
+int get(void){
     
-    char c = getchar(); 
-    
-    ungetc(c, stdin); 
-    
-    return c; 
+    int a;
+
+    //helpful link: https://stackoverflow.com/questions/28375031/ignore-whitespace-getchar
+    //removes the bug where it would print try to evaluate a new line which would obv. trigger an ERROR statement 
+    do {
+    a = getchar();
+    } while (a == (unsigned char) ' ' || a == (unsigned char) '\t' || a == (unsigned char) '\n');
+
+    return a;
 }
 
+int peek(void) { 
+    
+    char a;
+
+    do {
+    a = getchar();
+    } while (a == (unsigned char) ' ' || a == (unsigned char) '\t' || a == (unsigned char) '\n');
+
+    //note on using ungetc(c, std); here: If is placed here causes an infinite loop since the byte is placed back onto the stream.
+    //but if commented out program will end if enter two or more digit num.
+    ungetc(a, stdin); 
+    
+    return a; 
+}
+
+//eat whitespace()
 
 object *read(void){
 
     int a = peek();
+    
+    a = get();
     //if digit then...
+    
+    
     if(isdigit(a)){
         
-        return makeNum(a); 
-    
         
 
+        return makeNum(a); 
+    
 
     }
 
     //since its not a digit it must be just an error....
     return make_error(); 
+
 }
 
 /*      EVAL        */
@@ -80,12 +106,13 @@ object *eval(object *exp){
 }
 
 /*      WRITE       */
-int write(object *obj){
+void write(object *obj){
     
     if(obj->type == ERROR){
         
         printf("ERROR\n");
-        exit(1);
+        //exit(1);
+      //  return 1;
     }
 
     else{
@@ -93,7 +120,8 @@ int write(object *obj){
         printf("%d\n", obj->data.number.value);
     }
 
-    return 1;
+
+    //return 0;
 }
 
 
@@ -101,7 +129,24 @@ int write(object *obj){
 
 int main(void)
 {
-    int a = 0;
+    /*
+    int quit = 0;
+    int choice = 0;
+
+    do{
+        switch(choice){
+            case 0: 
+                choice = write(eval(read())); 
+                break;
+
+            case 1: 
+                printf("ERROR\n");
+                quit = 1;
+                break;
+        }
+    }while (quit == 0);
+*/
+/*
 
     do{
         
@@ -110,6 +155,15 @@ int main(void)
         printf("\n");
     
     }while(a == 1);
-
+*/
+/*
+    while(write(eval(read())) == 0){
+        
+        write(eval(read()));
+    }
+  */
+    while(1){
+    write(eval(read()));
+    };
     return 0;
 }
