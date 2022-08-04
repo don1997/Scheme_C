85ee4a9beb9cf4592a336ad4d5eece7dc00c9203
don1997 <donald_j97@hotmail.com>
2022-08-04 11:34:40 -0400

    finish pairs

--- main.c
+++ main.c
@@ -170,6 +170,35 @@ void set_cdr(object *obj, object *value){
     obj->data.pair.cdr =  value;
 }
 
+#define caar(obj)   car(car(obj))
+#define cadr(obj)   car(cdr(obj))
+#define cdar(obj)   cdr(car(obj))
+#define cddr(obj)   cdr(cdr(obj))
+#define caaar(obj)  car(car(car(obj)))
+#define caadr(obj)  car(car(cdr(obj)))
+#define cadar(obj)  car(cdr(car(obj)))
+#define caddr(obj)  car(cdr(cdr(obj)))
+#define cdaar(obj)  cdr(car(car(obj)))
+#define cdadr(obj)  cdr(car(cdr(obj)))
+#define cddar(obj)  cdr(cdr(car(obj)))
+#define cdddr(obj)  cdr(cdr(cdr(obj)))
+#define caaaar(obj) car(car(car(car(obj))))
+#define caaadr(obj) car(car(car(cdr(obj))))
+#define caadar(obj) car(car(cdr(car(obj))))
+#define caaddr(obj) car(car(cdr(cdr(obj))))
+#define cadaar(obj) car(cdr(car(car(obj))))
+#define cadadr(obj) car(cdr(car(cdr(obj))))
+#define caddar(obj) car(cdr(cdr(car(obj))))
+#define cadddr(obj) car(cdr(cdr(cdr(obj))))
+#define cdaaar(obj) cdr(car(car(car(obj))))
+#define cdaadr(obj) cdr(car(car(cdr(obj))))
+#define cdadar(obj) cdr(car(cdr(car(obj))))
+#define cdaddr(obj) cdr(car(cdr(cdr(obj))))
+#define cddaar(obj) cdr(cdr(car(car(obj))))
+#define cddadr(obj) cdr(cdr(car(cdr(obj))))
+#define cdddar(obj) cdr(cdr(cdr(car(obj))))
+#define cddddr(obj) cdr(cdr(cdr(cdr(obj))))
+
 //  Read() Helpers    //
 int peek(){
     int a;
@@ -320,11 +349,12 @@ object *read(void){
 
     char buffer[BUFFER_MAX];
 
-
+    short sign = 1;
     int num = 0;
     eat_whitespace();
     int a = getchar();
-    
+   
+    /*
     //reads number
     if(isdigit(a)){
          
@@ -332,9 +362,34 @@ object *read(void){
         while(isdigit(a = getchar()))
             num =  (num * 10) + (a - '0');
 
-        return makeNum(num);    
+        if(isdelim(a)){
+            ungetc(a, stdin);
+            return makeNum(num);
+        }
     }
-    
+    */
+
+    if (isdigit(a) || (a == '-' && (isdigit(peek())))) {
+        /* read a fixnum */
+        if (a == '-') {
+            sign = -1;
+        }
+        else {
+            ungetc(a, stdin);
+        }
+        while (isdigit(a = getchar())) {
+            num = (num * 10) + (a - '0');
+        }
+        num *= sign;
+        if (isdelim(a)) {
+            ungetc(a, stdin);
+            return makeNum(num);
+        }
+        else {
+            fprintf(stderr, "number not followed by delimiter\n");
+            exit(1);
+        }
+    }
     else if(a == '#'){
 
         a = getchar();
@@ -383,7 +438,7 @@ object *read(void){
     }
     else if(a == '('){
         
-        read_pair();
+       return read_pair();
     }
     else {
         fprintf(stderr, "bad input. Unexpected '%c'\n", a);
@@ -417,7 +472,6 @@ void write_pair(object *pair){
     cdr_obj = cdr(pair);
 
     write(car_obj);
-
     if(cdr_obj-> type == PAIR){
         printf(" ");
         write_pair(cdr_obj);
@@ -496,7 +550,6 @@ void write(object *obj){
             break;
 
         case PAIR:
-            
             printf("(");
             write_pair(obj);
             printf(")");
